6f9b134999a8f8a4251028a91c2e62435086e3dd
vitor.silva <vitor2071@gmail.com>
2023-04-12 20:04:37 -0300

    feature: implementa métodos de deposito e saque e complementa testes da classe

--- conta_bancaria.rb
+++ conta_bancaria.rb
@@ -1,6 +1,22 @@
 class ContaBancaria 
 
-    def initialize(saldo,conta)
+   
+    attr_accessor :saldo, :numero
 
+    def initialize(saldo, numero)
+        @saldo = saldo
+        @numero = numero
     end 
+
+    def deposito(valor)
+        valor > 0 ? @saldo+=valor : false 
+    end 
+
+    def saque(valor)
+        if @saldo > 0 && valor > 0 && valor <= @saldo  
+            @saldo-=valor
+        else 
+            false
+        end 
+    end
 end 
--- spec/conta_bancaria_spec.rb
+++ spec/conta_bancaria_spec.rb
@@ -4,15 +4,19 @@ describe "Banco" do
 
     it "deve criar uma conta bancaria" do 
         expect(conta01.saldo).to eq(2500)
-        expect(conta01.numero).to eq(001)
+        expect(conta01.numero).to eq(1)
     end 
 
     it "deve fazer deposito de valor na conta" do 
         expect(conta01.deposito(250)).to eq(2750)
+        expect(conta01.deposito(-10)).to eq(false)
     end 
 
     it "deve fazer saque de valor na conta" do 
-        expect(conta01.saque(500)).to eq(2250)
+        expect(conta01.saque(250)).to eq(2250)
+        expect(conta01.saque(200)).to eq(2050)
+        expect(conta01.saque(-5)).to eq(false)
+        expect(conta01.saque(10000)).to eq(false)
     end 
 end 
 
